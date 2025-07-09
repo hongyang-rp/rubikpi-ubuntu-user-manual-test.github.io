a99249a06b790876e7e6060d6c327bd72299a349
hongyang-rp <hongyang.zhao@thundersoft.com>
2025-07-09 20:46:16 +0800

    更改网页图标
    Signed-off-by: hongyang-rp <hongyang.zhao@thundersoft.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'RUBIK Pi Ubuntu24.04 user Guide',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
