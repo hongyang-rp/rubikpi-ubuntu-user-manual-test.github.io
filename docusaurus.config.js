8634bbcc1982dc60e69f5996a6cbfe08df360fe4
Chloe Chen <chloe.cc77085@gmail.com>
2025-08-12 13:58:45 +0800

    根据英文修改并重新编译
    Signed-off-by: Chloe Chen <chloe.cc77085@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,6 +151,12 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
-};
+    markdown: {
+      mermaid: true,
+    },
+    themes: ['@docusaurus/theme-mermaid'],
+
+  };
+
 
 export default config;
